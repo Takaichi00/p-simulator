b018f8b42825ec2b497c63836855bb900655795f
Tomoaki Takaichi <kiruhihhohu555@gmail.com>
2021-03-13 20:12:10 +0900

    feat & wip : Player first hit impl but has bug

--- api/src/test/java/com/takaichi00/domain/symphogear/SymphogearPlayerTest.java
+++ api/src/test/java/com/takaichi00/domain/symphogear/SymphogearPlayerTest.java
@@ -1,7 +1,8 @@
 package com.takaichi00.domain.symphogear;
 
 import org.junit.jupiter.api.Nested;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -15,31 +16,41 @@ class SymphogearPlayerTest {
   @Nested
   class Playerは1000円あたり20回転でシンフォチアを打つ能力をもつ場合 {
 
-    @Test
-    void 通常時のシンフォギアを大当りするまで打ち_初当たりにかかったお金と回転数を取得できる() {
-      // setup
-      SymphogearMachine symphogearMachine = spy(new SymphogearMachine());
+    private static final int ROUND_PER_1000YEN = 20;
 
-      List<Boolean> hitMockBoolean = new ArrayList<>();
+    @Nested
+    class 通常時のシンフォギアを大当りするまで打ち_初当たりにかかったお金と回転数を取得できる {
 
-      for (int i = 0; i < 198; ++i) {
-        hitMockBoolean.add(false);
+      @ParameterizedTest
+      @CsvSource({
+        "10000, 200",
+        "5000, 100",
+        "2500, 50"
+      })
+      void X円投資Y回転であたった場合(int firstHitMoney, int firstHitRound) {
+        // setup
+        SymphogearMachine symphogearMachine = spy(new SymphogearMachine());
+
+        List<Boolean> hitMockBoolean = new ArrayList<>();
+
+        for (int i = 0; i < firstHitRound - 2; ++i) {
+          hitMockBoolean.add(false);
+        }
+        hitMockBoolean.add(true);
+
+        when(symphogearMachine.drawLots()).thenReturn(false, hitMockBoolean.toArray(new Boolean[hitMockBoolean.size()]));
+
+        SymphogearPlayer testTarget = SymphogearPlayer.of(symphogearMachine, ROUND_PER_1000YEN);
+        FirstHitInformation expected = FirstHitInformation.of(firstHitMoney, firstHitRound);
+
+        // execute
+        testTarget.playSymphogear();
+        FirstHitInformation actual = testTarget.getFirstImformation();
+
+        // assert
+        assertEquals(expected.getFirstHitMoney(), actual.getFirstHitMoney());
+        assertEquals(expected.getFirstHitRound(), actual.getFirstHitRound());
       }
-      hitMockBoolean.add(true);
-
-      when(symphogearMachine.drawLots()).thenReturn(false, hitMockBoolean.toArray(new Boolean[hitMockBoolean.size()]));
-
-      SymphogearPlayer testTarget = SymphogearPlayer.of(symphogearMachine, 20);
-      FirstHitInformation expected = FirstHitInformation.of(10000, 200);
-
-      // execute
-      testTarget.playSymphogear();
-      FirstHitInformation actual = testTarget.getFirstImformation();
-
-      // assert
-      assertEquals(expected.getFirstHitMoney(), actual.getFirstHitMoney());
-      assertEquals(expected.getFirstHitRound(), actual.getFirstHitRound());
     }
-
   }
 }
