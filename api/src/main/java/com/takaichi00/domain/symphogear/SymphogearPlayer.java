bbfa82883a5fb69e2ebfe26868bf5c76593ccac8
Tomoaki Takaichi <kiruhihhohu555@gmail.com>
2021-03-17 00:16:40 +0900

    feat: impl player use money to get ball

--- api/src/main/java/com/takaichi00/domain/symphogear/SymphogearPlayer.java
+++ api/src/main/java/com/takaichi00/domain/symphogear/SymphogearPlayer.java
@@ -3,15 +3,21 @@ package com.takaichi00.domain.symphogear;
 import static java.math.RoundingMode.HALF_UP;
 
 import java.math.BigDecimal;
-import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 
 @EqualsAndHashCode
-@AllArgsConstructor
 public class SymphogearPlayer {
 
   private final SymphogearMachine symphogearMachine;
   private final int roundPer1000yen;
+
+  private int havingBall = 0;
+  private int useMoney = 0;
+
+  public SymphogearPlayer(SymphogearMachine symphogearMachine, int roundPer1000yen) {
+    this.symphogearMachine = symphogearMachine;
+    this.roundPer1000yen = roundPer1000yen;
+  }
 
   public static SymphogearPlayer of(SymphogearMachine symphogearMachine, int roundPer1000yen) {
     return new SymphogearPlayer(symphogearMachine, roundPer1000yen);
@@ -34,13 +40,15 @@ public class SymphogearPlayer {
   }
 
   public void getBallBy500Yen() {
+    havingBall += symphogearMachine.outBallBy500Yen();
+    useMoney += 500;
   }
 
   public int getHavingBall() {
-    return 125;
+    return havingBall;
   }
 
   public int getUseMoney() {
-    return 500;
+    return useMoney;
   }
 }
--- api/src/test/java/com/takaichi00/domain/symphogear/SymphogearPlayerTest.java
+++ api/src/test/java/com/takaichi00/domain/symphogear/SymphogearPlayerTest.java
@@ -33,6 +33,21 @@ class SymphogearPlayerTest {
       assertEquals(expectedMoney, actualMoney);
     }
 
+    @Test
+    void Playerは1000円をマシンに入れると所持玉と消費金額を取得できる() {
+      SymphogearPlayer testTarget = SymphogearPlayer.of(new SymphogearMachine(), ROUND_PER_1000YEN);
+      int expectedBall = 250;
+      int expectedMoney = 1000;
+
+      testTarget.getBallBy500Yen();
+      testTarget.getBallBy500Yen();
+      int actualBall = testTarget.getHavingBall();
+      int actualMoney = testTarget.getUseMoney();
+
+      assertEquals(expectedBall, actualBall);
+      assertEquals(expectedMoney, actualMoney);
+    }
+
     @Nested
     class 通常時のシンフォギアを大当りするまで打ち_初当たりにかかったお金と回転数を取得できる {
 
