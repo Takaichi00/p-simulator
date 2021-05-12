76b4d56890bdd88ca98c76bc79f44f7051fbb4bc
Tomoaki Takaichi <kiruhihhohu555@gmail.com>
2021-05-12 22:09:44 +0900

    chore: setup dbsetup (wip)

---
+++ api/src/main/java/com/takaichi00/domain/symphogear/SymphogearRepository.java
@@ -0,0 +1,4 @@
+package com.takaichi00.domain.symphogear;
+
+public interface SymphogearRepository {
+}
---
+++ api/src/main/java/com/takaichi00/infrastructure/symphogear/JdbcSymphogearRepository.java
@@ -0,0 +1,6 @@
+package com.takaichi00.infrastructure.symphogear;
+
+import com.takaichi00.domain.symphogear.SymphogearRepository;
+
+public class JdbcSymphogearRepository implements SymphogearRepository {
+}
